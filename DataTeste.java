3768f805d815cc923a0eee3411b2a6dd5ea642f0
rippires <64215193+rippires@users.noreply.github.com>
2022-06-28 19:30:18 -0300

    Add files via upload

---
+++ DataTeste.java
@@ -0,0 +1,18 @@
+package classe;
+
+public class DataTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Data d1 = new Data();
+		
+		d1.dia = 13;
+		d1.mes = 06;
+		d1.ano = 1990;
+		
+		System.out.printf("A data definida foi : %d/%d/%d", d1.dia,d1.mes,d1.ano);
+
+	}
+
+}
---
+++ PrimeiroTrauma.java
@@ -0,0 +1,16 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+	int a = 3;
+	
+	public static void main(String[] args) {
+		
+		
+		PrimeiroTrauma a = new PrimeiroTrauma();
+		
+		System.out.println(a.a);
+
+	}
+
+}
